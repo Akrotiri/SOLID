61e7feb373ba463947e605a894d3daee68c06880
Jens Pettersson <jens.pettersson@live.se>
2016-05-25 15:56:46 +0200

    Added a Presentation Solution with all projects ready to begin coding in

---
+++ src/_presentation/Dependency Inversion/Solid.Banker.Runner/SolidBankerRunner.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Solid.Banker.Runner
+{
+    [TestFixture, Explicit("This is just an example runner")]
+    public class SolidBankerRunner
+    {
+        [Test]
+        public void RunBanker()
+        {
+    
+        }
+    }
+}
